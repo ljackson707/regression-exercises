a268f7abfe731ec2afd2d6ac6e44ba97e0a2947c
Liam Jackson <ljackson3613@yahoo.com>
2021-03-16 12:35:08 -0500

    scale.py

--- scale.py
+++ scale.py
@@ -156,3 +156,23 @@ def quantile_transformer(train, validate, test):
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Fucntions that show set scaler, original, and scaled
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def visualize_scaled_date(scaler, scaler_name, feature):
+    train_scaled = scaler.fit_transform(train[[feature]])
+    fig = plt.figure(figsize = (12,6))
+
+    gs = plt.GridSpec(2,2)
+
+    ax1 = fig.add_subplot(gs[0, :])
+    ax2 = fig.add_subplot(gs[1,0])
+    ax3 = fig.add_subplot(gs[1,1])
+
+    ax1.scatter(train[[feature]], train_scaled)
+    ax1.set(xlabel = feature, ylabel = 'Scaled_' + feature, title = scaler_name)
+
+    ax2.hist(train[[feature]])
+    ax2.set(title = 'Original')
+
+    ax3.hist(scaled)
+    ax3.set(title = 'Scaled')
+    plt.tight_layout();
+
