c7902191986c72cd7b28b9b48792b5aa086b8c78
Liam Jackson <ljackson3613@yahoo.com>
2021-03-17 11:17:43 -0500

    evaluate

--- evaluate.py
+++ evaluate.py
@@ -74,3 +74,65 @@ def model_significance(df):
 
     df['yhat'] = ols_model.predict(df.total_bill)
     return ols_model.summary()
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+import math
+import sklearn.metrics
+import pandas as pd
+import numpy as  np
+import matplotlib.pyplot as plt
+
+def residuals(actual, predicted):
+    return actual - predicted
+
+def sse(actual, predicted):
+    return (residuals(actual, predicted) **2).sum()
+
+def mse(actual, predicted):
+    n = actual.shape[0]
+    return sse(actual, predicted) / n
+
+def rmse(actual, predicted):
+    return math.sqrt(mse(actual, predicted))
+
+def ess(actual, predicted):
+    return ((predicted - actual.mean()) ** 2).sum()
+
+def tss(actual):
+    return ((actual - actual.mean()) ** 2).sum()
+
+def regression_errors(actual, predicted):
+    return pd.Series({
+        'sse': sse(actual, predicted),
+        'ess': ess(actual, predicted),
+        'tss': tss(actual),
+        'mse': mse(actual, predicted),
+        'rmse': rmse(actual, predicted),
+    })
+
+def baseline_mean_errors(actual):
+    predicted = actual.mean()
+    return {
+        'sse': sse(actual, predicted),
+        'mse': mse(actual, predicted),
+        'rmse': rmse(actual, predicted),
+    }
+
+def better_than_baseline(actual, predicted):
+    rmse_baseline = rmse(actual, actual.mean())
+    rmse_model = rmse(actual, predicted)
+    return rmse_model < rmse_baseline
+
+def model_significance(ols_model):
+    return {
+        'r^2 -- variance explained': ols_model.rsquared,
+        'p-value -- P(data|model == baseline)': ols_model.f_pvalue,
+    }
+def plot_residuals(actual, predicted):
+    residuals = actual - predicted
+    plt.hlines(0, actual.min(), actual.max(), ls=':')
+    plt.scatter(actual, residuals)
+    plt.ylabel('residual ($y - \hat{y}$)')
+    plt.xlabel('actual value ($y$)')
+    plt.title('Actual vs Residual')
+    plt.show()
